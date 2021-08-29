14931574eac8b24e7fa063f01a85931a63146ee9
SuMu <SuMu31707@163.com>
2021-08-29 15:52:31 +0800

    feat: 默认分隔符\001的使用 DDL建表练习

---
+++ src/main/hive/02_hive_ddl/3_create_table.sql
@@ -0,0 +1,22 @@
+-- 三、默认分隔符的使用
+-- 文件team_ace_player.txt中记录了手游《王者荣耀》主要战队内最受欢迎的王牌选手信息，字段之间使用的是\001作为分隔符，要求在Hive中建表并成功映射该文件。
+-- 字段含义：id、team_name（战队名称）、ace_player_name（王牌选手名字）
+-- 数据都是原生数据类型，且字段之间分隔符为\001，因此在建表的时候可以省去row format语句，因为hive默认的分隔符就是\001
+
+-- 1、切换数据库
+use sumu;
+
+-- 2、建表
+create table t_team_ace_player(
+    id int comment "主键",
+    team_name string comment "战队名称",
+    ace_player_name string comment "王牌选手名字"
+) comment "王者手游主要战队王牌选手信息表"; -- 未指定row format语句，采用的是默认的\001作为字段之间的分隔符
+
+-- 3、查看表是否建成功
+show tables;
+
+-- 4、上传文件
+
+-- 5、查看表数据
+select * from t_team_ace_player;
